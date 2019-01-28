9d52d07f9ec70ce529cdc3eac9f76ab400a4610f
Grant Blakeman <hello@grantblakeman.com>
2019-01-28 11:07:49 -0800

    updates to v1 to fix bug + add more testing scenarios

--- src/App/Util/Util.php
+++ src/App/Util/Util.php
@@ -9,9 +9,9 @@ function get_template_directory() {
 
 class Util {
   public function getSvg($filename) {
-    $clean_filename = preg_replace('/((\/*views)*\/*(partials)*\/*(svg)\/)/', '', $filename);
+    $clean_filename = preg_replace('/(^\/*views\/partials\/svg\/)|(^\/*partials\/svg\/)|(^\/*svg\/)/', '', $filename);
     $clean_filename = ltrim($clean_filename, '/');
-    $clean_filename = basename($clean_filename, '.svg');
+    $clean_filename = preg_replace('/\.svg$/', '', $clean_filename);
     $full_path_with_filename = get_template_directory() . '/views/partials/svg/' . $clean_filename . '.svg';
 //    echo file_get_contents($full_path_with_filename);
     return $full_path_with_filename;
